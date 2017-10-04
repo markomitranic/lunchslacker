06af4e8656bdb6782429b0981213d5e4e1520703
Aleksandar Ilic <aleksandar.ilic@catenamedia.com>
2017-10-04 12:18:38 +0200

    Included Slack bundle into AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new CL\Bundle\SlackBundle\CLSlackBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
